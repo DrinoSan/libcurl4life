c1c6fc81a5ff89f9788ee6bf9fbb165fba299c2c
Sandrino <s.becirbegovic@gmail.com>
2021-10-25 16:39:31 +0200

    Initial commit

---
+++ curl_client.cpp
@@ -0,0 +1,139 @@
+//
+// Created by soellne2 on 22.10.2021.
+//
+
+#include "CurlClient.h"
+
+
+static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
+{
+    ((std::string*)userp)->append((char*)contents, size * nmemb);
+    return size * nmemb;
+}
+
+CurlClient_t::CurlClient_t() {
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+    init();
+}
+
+void CurlClient_t::init(){
+    if(curl_)
+    {
+        curl_easy_cleanup(curl_);
+    }
+    curl_ = curl_easy_init();
+
+    responseBody_.clear();
+}
+
+CurlClient_t::~CurlClient_t() {
+    curl_global_cleanup();
+}
+
+void CurlClient_t::setHeaders(std::vector<std::pair<std::string, std::string>>& values){
+    struct curl_slist *chunk = NULL;
+
+    for (auto &pair : values)
+    {
+        chunk = curl_slist_append(chunk, (pair.first + ": " + pair.second).c_str());
+
+    }
+    //chunk = curl_slist_append(chunk, ";");
+
+    curl_easy_setopt(curl_, CURLOPT_HTTPHEADER, chunk);
+}
+
+CurlClient_t::HttpResponse_t CurlClient_t::sendMessage(
+        const Method& method,
+        const std::string& uri,
+        const std::string& content,
+        const std::string& contentType,
+        const std::string& user,
+        const std::string& password,
+        const bool useSSL
+) {
+    HttpResponse_t result = HttpResponse_t();
+
+    init();
+
+
+    using headerPairs = std::vector<std::pair<std::string, std::string>>;
+    headerPairs requestHeader;
+
+
+
+    //set url
+    curl_easy_setopt(curl_, CURLOPT_URL, uri.c_str());
+
+    //set method
+    switch (method) {
+        case HTTP_GET:
+            curl_easy_setopt(curl_, CURLOPT_HTTPGET, 1L);
+            break;
+        case HTTP_POST:
+            curl_easy_setopt(curl_, CURLOPT_HTTPPOST, 1L);
+            requestHeader.push_back(std::make_pair("Content-Type", "application/json"));
+
+            //set request body
+            curl_easy_setopt(curl_, CURLOPT_POSTFIELDS, content.c_str());
+            break;
+        default:
+            //TODO error
+            curl_easy_setopt(curl_, CURLOPT_HTTPGET, 1L);
+            break;
+    }
+
+
+
+
+
+    //verify certificate
+    if (useSSL){
+        curl_easy_setopt(curl_, CURLOPT_SSL_VERIFYPEER, 1L);
+        curl_easy_setopt(curl_, CURLOPT_SSL_VERIFYHOST, 1L);
+    }else{
+        curl_easy_setopt(curl_, CURLOPT_SSL_VERIFYPEER, 0L);
+        curl_easy_setopt(curl_, CURLOPT_SSL_VERIFYHOST, 0L);
+    }
+
+    //follow redirect
+    curl_easy_setopt(curl_, CURLOPT_FOLLOWLOCATION, 1L);
+
+    //set request header
+    setHeaders(requestHeader);
+
+
+
+    //set port
+
+    //set auth
+
+    //TODO remove
+    //curl_easy_setopt (curl_, CURLOPT_VERBOSE, 1L);
+
+    //prepare callback
+    curl_easy_setopt(curl_, CURLOPT_WRITEFUNCTION, WriteCallback);
+    curl_easy_setopt(curl_, CURLOPT_WRITEDATA, &responseBody_);
+
+    curl_easy_setopt(curl_, CURLOPT_HEADERFUNCTION, WriteCallback);
+    curl_easy_setopt(curl_, CURLOPT_HEADERDATA, &responseHeader_);
+
+    //send message
+    CURLcode res = curl_easy_perform(curl_);
+
+    if(res == CURLE_OK){//TODO
+        result.body = responseBody_;
+        result.header = responseHeader_;
+        result.success = true;
+
+        int response_code;
+        curl_easy_getinfo(curl_, CURLINFO_RESPONSE_CODE, &response_code);
+        result.httpStatus = static_cast<Status>( response_code);
+    }
+
+    //cleanup
+    curl_easy_cleanup(curl_);
+
+    return  result;
+}
+
---
+++ main.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <curl/curl.h>
+#include "CurlClient.h"
+
+//using CurlClient_t;
+
+int main() {
+
+
+    CurlClient_t client = CurlClient_t();
+    CurlClient_t::HttpResponse_t sand= client.sendMessage(CurlClient_t::HTTP_POST, "https://sardino.free.beeceptor.com", "","","","", false);
+    //CurlClient_t::HttpResponse_t sand= client.sendMessage(CurlClient_t::HTTP_GET, "www.google.com", "","","","", false);
+
+    std::cout << sand.body << std::endl;
+
+    return 0;
+}
+
+//
+//size_t CurlWrite_CallbackFunc_StdString(void *contents, size_t size, size_t nmemb, std::string *s)
+//{
+//    size_t newLength = size*nmemb;
+//    try
+//    {
+//        s->append((char*)contents, newLength);
+//    }
+//    catch(std::bad_alloc &e)
+//    {
+//        //handle memory problem
+//        return 0;
+//    }
+//    return newLength;
+//}
+//
+//int main()
+//{
+//    CURL *curl;
+//    CURLcode res;
+//
+//    curl_global_init(CURL_GLOBAL_DEFAULT);
+//
+//    curl = curl_easy_init();
+//    std::string s;
+//    if(curl)
+//    {
+//
+//        //curl_easy_setopt(curl, CURLOPT_URL, "http://demo2996169.mockable.io/");
+//        //curl_easy_setopt(curl, CURLOPT_URL, "https://10.212.11.40/service/stations/transfer_stations");
+//        curl_easy_setopt(curl, CURLOPT_URL, "gorest.co.in:443/public/v1/users/3812");
+//
+//        //curl_easy_setopt(curl, CURLOPT_USERPWD, "admin:admin");
+//
+//        //follow redirect
+//        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+//
+//        //curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L); //only for https
+//        //curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L); //only for https
+//
+//        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, CurlWrite_CallbackFunc_StdString);
+//        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &s);
+//
+//        curl_easy_setopt (curl, CURLOPT_VERBOSE, 1L); //remove this to disable verbose output
+//        curl_easy_setopt(curl, CURLOPT_HEADER, 1L);
+//
+//
+//        /* Perform the request, res will get the return code */
+//        res = curl_easy_perform(curl);
+//        /* Check for errors */
+//        if(res != CURLE_OK)
+//        {
+//            fprintf(stderr, "curl_easy_perform() failed: %s\n",
+//                    curl_easy_strerror(res));
+//        }
+//
+//        /* always cleanup */
+//        curl_easy_cleanup(curl);
+//    }
+//
+//    std::cout<< "----------------------------Response Body Start \r\n" <<s<< "\r\n  --------------------------Response Body end"<<std::endl;
+//
+//    std::cout<< "Program finished!" << std::endl;
+//}
